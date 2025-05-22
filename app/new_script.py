937e08a1ea2d33e07c69cad7d70490bae4c4c294
unknown <hhamlaoui@biware-consulting.com>
2025-05-22 15:11:50 +0100

    Added new file

---
+++ app/new_script.py
@@ -0,0 +1,19 @@
+
+import requests
+
+base_url = "http://postgrest-api-demo:3000/orders_n"
+
+
+params = {
+    "id": "eq.1"
+}
+
+response = requests.get(base_url, params=params)
+
+if response.ok:
+    todos = response.json()
+    for todo in todos:
+        print(todo)
+else:
+    
+    print("Error:", response.status_code, response.text)
